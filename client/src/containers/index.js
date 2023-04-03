0d9f2a36105a8204f1b654ea650be04b1063f789
Manoel do Nascimento <manu3105@yahoo.com>
2023-04-03 20:21:00 -0300

    feat: base app done

---
+++ client/src/containers/index.js
@@ -0,0 +1,9 @@
+export { default as Home } from './Home'
+export { default as CreatePost } from './CreatePost'
+// import Home from './Home'
+// import CreatePost from './CreatePost'
+
+// export {
+//   Home,
+//   CreatePost
+// }
