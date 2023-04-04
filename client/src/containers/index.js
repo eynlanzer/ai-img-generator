92842b1325f91122a5969560711a4da021820e6d
Manoel do Nascimento <manu3105@yahoo.com>
2023-04-03 21:22:41 -0300

    feat: Home page + components folder

--- client/src/containers/index.js
+++ client/src/containers/index.js
@@ -1,9 +1,2 @@
 export { default as Home } from './Home'
 export { default as CreatePost } from './CreatePost'
-// import Home from './Home'
-// import CreatePost from './CreatePost'
-
-// export {
-//   Home,
-//   CreatePost
-// }
